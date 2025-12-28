1a0b8d93a04f36c73ede1437a737814106d79705
Imptovskii <mail@imptovskii.ru>
2025-12-29 01:16:55 +0300

    modified:   src/main/java/dev/newplaces/connectionmsgs/ConnectionMsgsPlugin.java

--- src/main/java/dev/newplaces/connectionmsgs/ConnectionMsgsPlugin.java
+++ src/main/java/dev/newplaces/connectionmsgs/ConnectionMsgsPlugin.java
@@ -1,11 +1,13 @@
 package dev.newplaces.connectionmsgs;
 
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.UUID;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,66 +16,102 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ConnectionMsgsPlugin extends JavaPlugin implements Listener {
-   private HashSet<Player> on = new HashSet();
+    private HashSet<UUID> enabledPlayers = new HashSet<>();
+    private FileConfiguration config;
 
-   public void onEnable() {
-      Iterator var1 = Bukkit.getServer().getOnlinePlayers().iterator();
+    @Override
+    public void onEnable() {
+        // Generate default config
+        saveDefaultConfig();
+        config = getConfig();
+        setupConfigDefaults();
+        
+        // Load enabled players from previous session
+        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+            enabledPlayers.add(player.getUniqueId());
+        }
+        
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        
+        // Register command executor
+        getCommand("toggleconnectionmsgs").setExecutor(this);
+    }
 
-      while(var1.hasNext()) {
-         Player onlinePlayer = (Player)var1.next();
-         this.on.add(onlinePlayer);
-      }
+    private void setupConfigDefaults() {
+        config.addDefault("messages.toggle-off", "&aСообщения подключения были скрыты.");
+        config.addDefault("messages.toggle-on", "&aСообщения подключения снова видны.");
+        config.addDefault("messages.join", "&7%player% &fприсоединился к серверу");
+        config.addDefault("messages.quit", "&7%player% &fотключился от сервера");
+        config.addDefault("messages.console-error", "&cЭта команда может быть использована только игроком в игре.");
+        
+        config.options().copyDefaults(true);
+        saveConfig();
+    }
 
-      Bukkit.getServer().getPluginManager().registerEvents(this, this);
-   }
-
-   @EventHandler
-   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-      if (sender instanceof Player && command.getName().equalsIgnoreCase("toggleconnectionmsgs")) {
-         Player player = (Player)sender;
-         if (this.on.contains(player)) {
-            this.on.remove(player);
-            player.sendMessage(ChatColor.GOLD + "Сообщения подключения были скрыты.");
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!command.getName().equalsIgnoreCase("toggleconnectionmsgs")) {
+            return false;
+        }
+        
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', 
+                config.getString("messages.console-error", "&cЭта команда может быть использована только игроком в игре.")));
             return true;
-         } else {
-            this.on.add(player);
-            player.sendMessage(ChatColor.GOLD + "Сообщения подключения снова видны.");
-            return true;
-         }
-      } else {
-         return false;
-      }
-   }
+        }
 
-   @EventHandler
-   public void OnJoin(PlayerJoinEvent event) {
-      Player player = event.getPlayer();
-      this.on.add(player);
-      event.setJoinMessage((String)null);
-      Iterator var3 = Bukkit.getServer().getOnlinePlayers().iterator();
+        Player player = (Player) sender;
+        UUID playerId = player.getUniqueId();
 
-      while(var3.hasNext()) {
-         Player onlinePlayer = (Player)var3.next();
-         if (this.on.contains(onlinePlayer)) {
-            onlinePlayer.sendMessage(ChatColor.GRAY + player.getName() + " присоединился к серверу");
-         }
-      }
+        if (enabledPlayers.contains(playerId)) {
+            enabledPlayers.remove(playerId);
+            player.sendMessage(getFormattedMessage("messages.toggle-off"));
+        } else {
+            enabledPlayers.add(playerId);
+            player.sendMessage(getFormattedMessage("messages.toggle-on"));
+        }
+        return true;
+    }
 
-   }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        enabledPlayers.add(player.getUniqueId());
+        event.setJoinMessage(null);
 
-   @EventHandler
-   public void OnQuit(PlayerQuitEvent event) {
-      Player player = event.getPlayer();
-      this.on.remove(player);
-      event.setQuitMessage((String)null);
-      Iterator var3 = Bukkit.getServer().getOnlinePlayers().iterator();
+        String joinMessage = getFormattedMessage("messages.join", player.getName());
+        
+        for (Player onlinePlayer : Bukkit.getServer().getOnlinePlayers()) {
+            if (enabledPlayers.contains(onlinePlayer.getUniqueId())) {
+                onlinePlayer.sendMessage(joinMessage);
+            }
+        }
+    }
 
-      while(var3.hasNext()) {
-         Player onlinePlayer = (Player)var3.next();
-         if (this.on.contains(onlinePlayer)) {
-            onlinePlayer.sendMessage(ChatColor.GRAY + player.getName() + " отключился от сервера");
-         }
-      }
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        UUID playerId = player.getUniqueId();
+        
+        if (enabledPlayers.contains(playerId)) {
+            enabledPlayers.remove(playerId);
+        }
+        event.setQuitMessage(null);
 
-   }
+        String quitMessage = getFormattedMessage("messages.quit", player.getName());
+        
+        for (Player onlinePlayer : Bukkit.getServer().getOnlinePlayers()) {
+            if (enabledPlayers.contains(onlinePlayer.getUniqueId())) {
+                onlinePlayer.sendMessage(quitMessage);
+            }
+        }
+    }
+
+    private String getFormattedMessage(String path, String... replacements) {
+        String message = config.getString(path, "");
+        for (int i = 0; i < replacements.length; i += 2) {
+            message = message.replace(replacements[i], replacements[i + 1]);
+        }
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
 }
